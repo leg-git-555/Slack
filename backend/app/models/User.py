e473d6965b50429a1e64ddef5fe1343fb1da8cb7
NickyViNi <happylemonlcm@gmail.com>
2024-02-13 10:56:27 -0800

    add User model

--- backend/app/models/User.py
+++ backend/app/models/User.py
@@ -1,6 +1,17 @@
-
-
+from datetime import datetime
 from .db import db
 
+
 class User(db.Model):
+
     __tablename__="users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    user_name = db.Column(db.String(50), nullable=False)
+    hashed_password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    profile_image_url = db.Column(db.String)
+    created_at = db.Column(db.Date, default=datetime.now)
+    updated_at = db.Column(db.Date, default=datetime.now, onupdate=datetime.now)
--- backend/app/models/db.py
+++ backend/app/models/db.py
@@ -1,16 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-def test():
-    pass
-
-def test2():
-    pass
-def test3():
-    pass
-
-def test6():
-    pass
-def test5():
-    pass
